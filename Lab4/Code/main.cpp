856c6f7ab43d1d89937cc0450c630624da2dcc0e
Michael Kwok <michael.kwok00@gmail.com>
2022-03-17 14:04:02 -0600

    Reformat

--- Lab4/Code/main.cpp
+++ Lab4/Code/main.cpp
@@ -2,16 +2,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  int myrank, npes;
-  printf("Hello from process %d out of %d\n", myrank, npes);
-
   MPI::Init(argc, argv);
 
-  npes = MPI::COMM_WORLD.Get_size();
-  myrank = MPI::COMM_WORLD.Get_rank();
+  int world_size = MPI::COMM_WORLD.Get_size();
+  int world_rank = MPI::COMM_WORLD.Get_rank();
 
-  printf("Hello from process %d out of %d\n", myrank, npes);
+  printf("Hello from process %d out of %d\n", world_rank, world_size);
   MPI::Finalize();
-  printf("Hello from process %d out of %d\n", myrank, npes);
   return 0;
 }
