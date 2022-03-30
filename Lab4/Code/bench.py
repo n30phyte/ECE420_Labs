1cb8df5187767e39b46c871a25b93cc4f988edd3
Michael Kwok <michael.kwok00@gmail.com>
2022-03-30 12:45:40 -0600

    Fix script

--- Lab4/Code/bench.py
+++ Lab4/Code/bench.py
@@ -9,23 +9,30 @@ subprocess.call(["make"])
 threads = ["1", "2", "4", "5", "10", "20"]
 sizes = ["5300", "13000", "18789"]
 
-for size in sizes:
-    subprocess.call(["./datatrim", "-b", size])
+with open("hosts") as hosts_file:
+    lines = hosts_file.readlines()
+    lines.pop(0)
 
-    for thread in threads:
-        subprocess.call(
-            [
-                "mpirun",
-                "-np",
-                "4",
-                # "-f", "hosts"
-                "./main",
-            ]
-        )
-        result = subprocess.call(["./serialtester"])
+    for size in sizes:
+        subprocess.call(["./datatrim", "-b", size])
 
-        if result != 0:
-            print("Error for thread count: " + thread + " size: " + size)
-            break
+        for host in lines:
+            subprocess.call("scp * %s:~" % host.strip(), shell=True)
 
-        os.rename("./data_output", "./data_output_" + thread + "_" + size)
+        for thread in threads:
+            subprocess.call(
+                [
+                    "mpirun",
+                    "-np",
+                    thread,
+                    "-f", "hosts",
+                    "./main",
+                ]
+            )
+            result = subprocess.call(["./serialtester"])
+
+            if result != 0:
+                print("Error for thread count: " + thread + " size: " + size)
+                break
+
+            os.rename("./data_output", "./data_output_" + thread + "_" + size)
