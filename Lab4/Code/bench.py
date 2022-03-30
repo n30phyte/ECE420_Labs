0c3bcd1274bdad7c48e5fca916fb8b9e1615aa48
Michael Kwok <michael.kwok00@gmail.com>
2022-03-29 19:19:19 -0600

    Improve bench script

--- Lab4/Code/bench.py
+++ Lab4/Code/bench.py
@@ -6,18 +6,26 @@ import subprocess
 subprocess.call(["make", "clean"])
 subprocess.call(["make"])
 
+threads = ["1", "2", "4", "5", "10", "20"]
 sizes = ["5300", "13000", "18789"]
 
 for size in sizes:
     subprocess.call(["./datatrim", "-b", size])
-    subprocess.call(["mpirun",
-    "-np", "4",
-    "-f", "hosts"
-    "./main"])
-    result = subprocess.call(["./serialtester"])
-    
-    if result != 0:
-        print("Error for size: " + size)
-        break
 
-    os.rename("./data_output", "./data_output_" + size)
+    for thread in threads:
+        subprocess.call(
+            [
+                "mpirun",
+                "-np",
+                "4",
+                # "-f", "hosts"
+                "./main",
+            ]
+        )
+        result = subprocess.call(["./serialtester"])
+
+        if result != 0:
+            print("Error for thread count: " + thread + " size: " + size)
+            break
+
+        os.rename("./data_output", "./data_output_" + thread + "_" + size)
